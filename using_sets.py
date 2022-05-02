2f0a3dfbe20259568efd9d459d474577a7c1bd9e
Mikal Williams <98860058+Mikeyisme@users.noreply.github.com>
2022-05-02 00:35:26 -0500

    Add files via upload

---
+++ using_sets.py
@@ -0,0 +1,20 @@
+# numbers_set = {1, 2, 3, 4, 4} # duplicate values removed
+# numbers_set = {1, 2, 3, 4, [5, 6]} # cannot use mutable data types
+# numbers_set = {1, 2, 3, 4, (5, 6)} # tuples are immutable, ok to use!
+# print(numbers_set)
+
+words_set = {"Alpha", "Bravo", "Charlie"}
+abcd = ""
+for word in words_set:
+    abcd += word
+print(abcd)
+if "Aplha" in words_set:
+    print("Alpha is in set")
+else:
+    print("Alpha not in set")
+
+words_set.add("Delta")
+print(words_set)
+words_set.discard("Bravo")
+
+wor = {[False, True], [True, True], [True, False], [False, False]}
